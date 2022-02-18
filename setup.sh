8bf0ec6d09e10e0601c5e808c6dc19f4507e202e
Thiago Sposito <thiago@sposito.ch>
2022-02-18 17:53:04 -0300

    update setup script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}' && cp ../.latexmkrc_ci .latexmkrc" \;
+find . -maxdepth 1 -type d \( ! -name . \) -exec bash -c "cd '{}' && cp ../.latexmkrc_ci .latexmkrc && cp ../*.sty . && cp ../sbc.bst ." \;
